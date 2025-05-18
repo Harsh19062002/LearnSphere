10f0ab05189c2cd3806063ca6d1fb205a8ba4602
Harsh19062002 <harshmahajan1962002@gmail.com>
2025-05-18 16:10:44 +0530

    commit

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,10 @@ const app = express();
 
 
 app.use(express.json()); 
-app.use(cors());
+app.use(cors({
+  origin: 'https://learn-sphere-frontend-two.vercel.app', 
+  credentials: true,
+}));
 
 
 connectdb();
